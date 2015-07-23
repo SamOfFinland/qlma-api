dc50f17373d6d6d51ee9de1e6210a449ac400635
Iiro Uusitalo <iiro.uusitalo@solita.fi>
2015-07-23 22:37:12 +0300

    Migratus lisätty

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
-                 [korma "0.4.2"]]
+                 [korma "0.4.2"]
+                 [migratus "0.8.2"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler qlma.handler/app}
   :profiles
