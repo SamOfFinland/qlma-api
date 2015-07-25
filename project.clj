d2040de8377e31cf8f8d89e481030cd4836872f1
Iiro Uusitalo <iiro.uusitalo@solita.fi>
2015-07-25 15:33:08 +0300

    Pakettien päivitys

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
                  [korma "0.4.2"]
                  [migratus "0.8.2"]
-                 [mysql/mysql-connector-java "5.1.6"]
+                 [mysql/mysql-connector-java "5.1.36"]
                  [selmer "0.8.6"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler qlma.handler/app}
