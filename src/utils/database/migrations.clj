221090b74396a14f9bc50a31775400f78f2438c0
Iiro Uusitalo <iiro.uusitalo@solita.fi>
2015-07-31 01:08:14 +0300

    Migrate skripti käyttämään yleisiä asetuksia

--- src/utils/database/migrations.clj
+++ src/utils/database/migrations.clj
@@ -1,11 +1,14 @@
 (ns utils.database.migrations
-  (:require [migratus.core :as migratus]))
+  (:require [migratus.core :as migratus]
+            [qlma.settings :as settings]))
 
-(def config {:store                 :database
+(def config
+  (let [db-params (-> (settings/get-settings) (:db))]
+            {:store                 :database
              :migration-dir         "migrations/"
              :migration-table-name  "migration"
              :db {:classname        "com.mysql.jdbc.Driver"
                   :subprotocol      "mysql"
-                  :subname          "//localhost/qlma"
-                  :user             "root"
-                  :password         ""}})
+                  :subname          (str "//" (:host db-params) "/" (:db db-params))
+                  :user             (:user db-params)
+                  :password         (:password db-params)}}))
