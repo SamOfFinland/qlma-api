f48739bd49e40952b1ab2ec66bad2ac10d152cd0
Iiro Uusitalo <iiro.uusitalo@solita.fi>
2015-08-02 07:56:23 +0300

    SQL muutoksia

--- resources/migrations/20150801232231-create-messages.up.sql
+++ resources/migrations/20150801232231-create-messages.up.sql
@@ -6,6 +6,10 @@ CREATE TABLE IF NOT EXISTS `messages` (
   PRIMARY KEY (`id`)  COMMENT '',
 CONSTRAINT `fk_to_user_id`
                 FOREIGN KEY (to_user_id) REFERENCES users (id)
-                ON DELETE CASCADE
-                ON UPDATE RESTRICT)
+                ON DELETE NO ACTION
+                ON UPDATE NO ACTION,
+CONSTRAINT `fk_from_user_id`
+                FOREIGN KEY (from_user_id) REFERENCES users (id)
+                ON DELETE NO ACTION
+                ON UPDATE NO ACTION)
 ENGINE = InnoDB;
