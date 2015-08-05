9230109cbc07bddc60ebfc4bb2a4e3b73f5811cc
Iiro Uusitalo <iiro.uusitalo@solita.fi>
2015-08-05 21:10:20 +0300

    Käyttäjäluonti funktio lisätty

--- src/qlma/db/users.clj
+++ src/qlma/db/users.clj
@@ -3,4 +3,13 @@
             [korma.core :as sql]))
 
 (defn get-all-users []
+  "Haetaan kaikki käyttäjät käyttäjä taulusta"
   (sql/select users))
+
+(defn create-user
+  "Luodaan käyttäjä tauluun"
+  [{:keys [username firstname lastname password]}]
+  (sql/insert users (sql/values {:username "jee"
+                                :firstname "jou"
+                                :lastname "moi"
+                                :password "tataa"})))
