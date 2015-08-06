1c3ecb3934449da6d3afd018032ff4904cccb01b
Iiro Uusitalo <iiro.uusitalo@solita.fi>
2015-08-06 21:06:17 +0300

    Metosinin ring-swagger mukaan ja hiccup

--- project.clj
+++ project.clj
@@ -2,13 +2,16 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [
+                 [org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
                  [korma "0.4.2"]
                  [migratus "0.8.2"]
                  [mysql/mysql-connector-java "5.1.36"]
-                 [selmer "0.8.7"]]
+                 [hiccup "1.0.5"]
+                 [metosin/ring-swagger "0.20.4"]
+                 ]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler qlma.handler/app}
   :profiles
