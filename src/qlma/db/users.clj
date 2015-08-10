4e6c044998f6bfd4fe17dc06c211fc8e9e7be6af
Iiro Uusitalo <iiro.uusitalo@solita.fi>
2015-08-10 22:53:35 +0300

    Käyttäjän kirjautumistietojen tarkistaminen

--- src/qlma/db/users.clj
+++ src/qlma/db/users.clj
@@ -1,6 +1,7 @@
 (ns qlma.db.users
   (:require [qlma.db.common :refer :all]
-            [korma.core :as sql]))
+            [korma.core :as sql]
+            [buddy.hashers :as password]))
 
 (defn get-all-users []
   "Haetaan kaikki käyttäjät käyttäjä taulusta"
@@ -12,4 +13,15 @@
   (sql/insert users (sql/values {:username username
                                 :firstname firstname
                                 :lastname lastname
-                                :password password})))
+                                :password (password/encrypt password)})))
+
+(defn user-and-password-ok?
+  "Tarkista käyttäjänimi ja salasana"
+  [username password]
+  (password/check password (get-user-password username)))
+
+(defn- get-user-password
+  "Haetaan käyttäjän salasana tietokannasta tarkistusta varten"
+  [username]
+  (-> (sql/select users (sql/fields [:password]) (sql/where {:username username})) first :password))
+
