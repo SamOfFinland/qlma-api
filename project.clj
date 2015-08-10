473a06664fbd73400f8972573d1d68d6d3bb0dce
Iiro Uusitalo <iiro.uusitalo@solita.fi>
2015-08-10 22:04:13 +0300

    Salasanakirjasto mukaan

--- project.clj
+++ project.clj
@@ -11,6 +11,7 @@
                  [mysql/mysql-connector-java "5.1.36"]
                  [hiccup "1.0.5"]
                  [metosin/ring-swagger "0.20.4"]
+                 [crypto-password "0.1.3"]
                  ]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler qlma.handler/app}
