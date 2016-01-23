707e2f66321ffafe576858caa7ae2d03ba9f924f
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-01-23 02:42:32 +0200

    fix index.html test

--- test/qlma/handler_test.clj
+++ test/qlma/handler_test.clj
@@ -1,13 +1,15 @@
 (ns qlma.handler-test
   (:require [clojure.test :refer :all]
+            [clojure.string :as s]
             [ring.mock.request :as mock]
             [qlma.handler :refer :all]))
 
 (deftest test-app
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
+      (println response)
       (is (= (:status response) 200))
-      (is (= (:body response) "Qlma Api"))))
+      (is (.contains (str (:body response)) "index.html"))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
