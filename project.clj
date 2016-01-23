19557ba00cc52373e73ff378bda1c703ceee4b96
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-01-23 03:38:22 +0200

    update lein-ring

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
                  [ring/ring-defaults "0.1.5"]
                  [ring-cors "0.1.7"]]
   :plugins [
-            [lein-ring "0.8.13"]
+            [lein-ring "0.9.7"]
             [lein-environ "1.0.1"]]
   :ring {:handler qlma.handler/app
          :init    utils.database.migrations/migrate}
